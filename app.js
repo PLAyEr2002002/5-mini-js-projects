49f14249957b80c1fee075d48a810db8be46bcde
PLAyEr2002002 <95416181+PLAyEr2002002@users.noreply.github.com>
2024-02-17 21:00:13 +0300

    added functions

---
+++ app.js
@@ -0,0 +1,23 @@
+let items=[]
+
+const itemsDiv= document.getElementById('items')
+
+function renderItems() {
+  itemsDiv.innerHTML = ''; // Clear the items container
+  
+}
+
+function loadItems() {
+  
+  }
+
+function saveItems() {
+}
+
+function addItem(item) {
+  
+}
+
+function removeItem(index) {
+
+}
